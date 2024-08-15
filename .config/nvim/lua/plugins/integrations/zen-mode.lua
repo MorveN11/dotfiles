b084f6326b050879ee1b07d35974afcbc8c8e6c2
MorveN11 <morales.patty.jose@gmail.com>
2024-08-14 23:05:33 -0400

    imporve zen mode configurations

--- .config/nvim/lua/plugins/integrations/zen-mode.lua
+++ .config/nvim/lua/plugins/integrations/zen-mode.lua
@@ -1,10 +1,17 @@
+-- Zen Mode configurations
 return {
     "folke/zen-mode.nvim",
     cmd = "ZenMode",
+
     opts = {
         plugins = {
             gitsigns = true,
+            wezterm = {
+                enabled = true,
+                font = "+4",
+            },
         },
     },
+
     keys = { { "<leader>z", "<CMD>ZenMode<CR>", desc = "Zen Mode" } },
 }
